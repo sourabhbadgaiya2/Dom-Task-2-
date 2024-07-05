a13c40752caa709514f87a29d35cf0784b7adeb1
sourabhbadgaiya2 <sourabhbadgaiya2@gmail.com>
2024-07-06 00:58:40 +0530

    task2

---
+++ script.js
@@ -0,0 +1,20 @@
+const imgs = document.querySelectorAll('.img');
+const img = document.querySelectorAll('.img img');
+
+const overlay = document.querySelector('.overlay');
+const overlayImg = document.querySelector('.overlay img');
+const box = document.querySelector('.box');
+
+// imgs.addEventListener('click', () => {
+//   overlay.style.zIndex = '1';
+//   box.style.opacity = 1;
+//   overlayImg.src = img.src;
+// });
+
+imgs.forEach((div, index) => {
+  div.addEventListener('click', () => {
+    overlay.style.zIndex = '1';
+    box.style.opacity = '1';
+    overlayImg.src = img[index].src;
+  });
+});
